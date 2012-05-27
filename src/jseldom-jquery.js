c3602e4105e088c509b0f5ac1b4e4c6188bb4b30
Wong Shek Hei Felix <shekhei@gmail.com>
2012-05-27 16:09:28 +0800

    modifying namespace of jseldom

--- src/jseldom-jquery.js
+++ src/jseldom-jquery.js
@@ -2,7 +2,7 @@
 Copyright (c) 2010, All Right Reserved, Wong Shek Hei @ shekhei@gmail.com
 License: GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
 **/
-(function() {
+(function(jQuery) {
 	var expr = /[.#\w].([\S]*)/g;
 	var classexpr = /(?!(\[))(\.)[^.#[]*/g;
 	var idexpr = /(#)[^.#[]*/;
@@ -35,7 +35,7 @@ License: GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.htm
 					varbuff = [];
 					isVar = false;
 				}
-				else if ( c !== ']' ) {
+				else if ( c !== ']' || quote !== '' ) {
 					if ( quote === '' && c === ',' ) {
 						buffer.val.push(varbuff.join(''));
 						varbuff = [];
@@ -129,76 +129,83 @@ License: GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.htm
 		if ( typeof count === "undefined" ) {
 			count = 1;
 		}
-		var eachfunc = function() {
-			var addFunc = rmFromParent(this);
-			var parent = [];
-			var lastparent = [];
-			for ( var x = 0; x < count; x++ ) {
-				parent.push(this);
-			}
-			lastparent = parent.slice();
-			//var buff = [];
-			for ( var i = 0; i < arr.length; i++ ) {
-				if ( arr[i].tag == '+' ) { parent = lastparent.slice();}
-				else {
-					for ( var x = 0; x < count; x++ ) {
-						if ( arr[i].tag == 'input' ) { // special case, needs to create tag by html
-							var html = [];
-							html.push( "<"+arr[i].tag);
-							if ( arr[i].id) {
-								html.push( "id=\'"+arr[i].id+"\'");
+		var returns = [];
+		var parent = [];
+		var lastparent = [];
+		var tempholder = document.createElement('div');
+		//var buff = [];
+		var depth = 0;
+		for ( var i = 0; i < arr.length; i++ ) {
+			if ( arr[i].tag == '+' ) { parent = lastparent.slice(); --depth;}
+			else {
+				for ( var x = 0; x < count; x++ ) {
+					if ( arr[i].tag == 'input' ) { // special case, needs to create tag by html
+						var html = [];
+						html.push( "<"+arr[i].tag);
+						if ( arr[i].id) {
+							html.push( "id=\'"+arr[i].id+"\'");
+						}
+						if ( arr[i].className ) {
+							html.push( "class=\'"+arr[i].className );
+							if ( i+1 === arr.length ) {
+								html.push( lastClass);
 							}
-							if ( arr[i].className ) {
-								html.push( "class=\'"+arr[i].className );
-								if ( i+1 === arr.length ) {
-									html.push( lastClass);
-								}
-								html.push( "\'");
+							html.push( "\'");
+						}
+						if ( arr[i].vars ) {
+							for ( var j = 0; j < arr[i].vars.length; j++ ) {
+								html.push(arr[i].vars[j][0]+"=\'"+arr[i].vars[j][1]+"\'");
 							}
-							if ( arr[i].vars ) {
-								for ( var j = 0; j < arr[i].vars.length; j++ ) {
-									html.push(arr[i].vars[j][0]+"=\'"+arr[i].vars[j][1]+"\'");
-								}
-							}
-							if ( arr[i].text ) {
-								html.push("text=\'"+arr[i].text+"\'");
-							}
-							html.push("/>");
-							lastparent[x] = parent[x];
+						}
+						if ( arr[i].text ) {
+							html.push("value=\'"+arr[i].text+"\'");
+						}
+						html.push("/>");
+						lastparent[x] = parent[x];
+						if ( !parent[x] ) {
+							tempholder.innerHTML = html.join(" ");
+							parent[x] = tempholder.removeChild(tempholder.firstChild);
+						} else {
 							parent[x].innerHTML = parent[x].innerHTML+html.join(" ");
 							parent[x]=parent[x].lastChild;
 						}
-						else {
-							var buff = document.createElement(arr[i].tag);
-							if ( arr[i].vars ) {
-								for ( var j = 0; j < arr[i].vars.length; j++ ) {
-									buff.setAttribute(arr[i].vars[j][0], arr[i].vars[j][1]);
-								}
+					}
+					else {
+						var buff = document.createElement(arr[i].tag);
+						if ( arr[i].vars ) {
+							for ( var j = 0; j < arr[i].vars.length; j++ ) {
+								console.log(arr[i].tag, arr[i].vars[j])
+								buff.setAttribute(arr[i].vars[j][0], arr[i].vars[j][1]);
 							}
-							if ( arr[i].id ) {
-								buff.id = arr[i].id;
-							}
-							if ( arr[i].className ) {
-								buff.className = arr[i].className;
-							}
-							if ( arr[i].text ) {
-								buff.appendChild(document.createTextNode(arr[i].text));
-							}
-							lastparent[x] = parent[x];
+						}
+						if ( arr[i].id ) {
+							buff.id = arr[i].id;
+						}
+						if ( arr[i].className ) {
+							buff.className = arr[i].className;
+						}
+						if ( arr[i].text ) {
+							buff.appendChild(document.createTextNode(arr[i].text));
+						}
+						lastparent[x] = parent[x];
+						if ( parent[x] ) {
 							parent[x] = parent[x].appendChild(buff);
+						} else {
+							parent[x] = buff;
 						}
 					}
 				}
-				newel = $.merge(newel, parent);
+				if ( !depth++ ) {
+					Array.prototype.push.apply( returns, parent );
+				}
 			}
-			addFunc(this);
-		};
-		this.each(eachfunc);
-		return $(newel);
+			newel = $.merge(newel, parent);
+		}
+		return $(returns);
 	};
 	var arrVer = function(sel, arr, count ) {
 		var regex = /%[^%]*%/g;
-		var vars = sel.match(regex);
+		var vars = sel.match(regex) || [];
 		var newel = [];
 		for ( var i = 0; i < arr.length; i++ ) {
 			var tsel = sel;
@@ -206,16 +213,19 @@ License: GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.htm
 				var vname = vars[j].substr(1,vars[j].length-2);
 				tsel = tsel.replace(vars[j], arr[i][vname]);
 			}
-			newel = $.merge(newel, nonArrVer.apply(this, [tsel, count]));
+			newel = $.merge(newel, nonArrVer(tsel, count));
 		}
 		return $(newel);
 	};
-	$.fn.jseldom = function(selector) {
-		if ( arguments.length == 1 || (arguments.length == 2 && !$.isArray(arguments[1]))) {
+	$.jseldom = function(selector) {
+		if ( arguments.length == 2 && $.isPlainObject(arguments[1])) {
+			return arrVer.apply(this, [arguments[0], [arguments[1]]]);
+		}
+		else if ( arguments.length == 1 || (arguments.length == 2 && !$.isArray(arguments[1]))) {
 			return nonArrVer.apply( this, arguments );
 		}
 		else if ( arguments.length == 2 ) {
-			return arrVer.apply(this,arguments);
+			return arrVer.apply( this, arguments);
 		}
 	}
-})();
+})(jQuery);
